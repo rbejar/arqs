30cfbf85432f935ea127f73536e0d2f03d0623b1
rbejar <rbejar@unizar.es>
2015-01-12 16:28:06 +0100

    Retoques en la indentación

--- collection_pipeline_java/src/main/java/ejemplo/Pipeline.java
+++ collection_pipeline_java/src/main/java/ejemplo/Pipeline.java
@@ -19,8 +19,8 @@ public class Pipeline {
 				.stream() // Las operaciones agregadas en Java 8 se hacen sobre streams, no sobre colecciones
 				.filter(s -> s.length() % 2 == 0) // Quedarnos con las de longitud par						
 				.map(String::toUpperCase) // Pasar a mayúsculas
-    			.sorted() // Ordenar después de pasar a mayúsculas, para que sea orden alfabético
-    			.collect(Collectors.joining()); // Convertir en String concatenando las que quedan    	
+				.sorted() // Ordenar después de pasar a mayúsculas, para que sea orden alfabético
+				.collect(Collectors.joining()); // Convertir en String concatenando las que quedan    	
 		System.out.println(result);
 	}
 }
