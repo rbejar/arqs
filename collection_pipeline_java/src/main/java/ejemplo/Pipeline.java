25d72d767b7ff684cd64e5f96b450409832d72aa
rbejar <rbejar@unizar.es>
2015-01-12 16:20:17 +0100

    Retoques en la indentación

--- collection_pipeline_java/src/main/java/ejemplo/Pipeline.java
+++ collection_pipeline_java/src/main/java/ejemplo/Pipeline.java
@@ -11,16 +11,16 @@ import java.util.stream.Collectors;
  * @author rbejar
  *
  */
-public class Pipeline {	    
-
-    public static void main(String[] argv) throws Exception {
-    	List<String> list = Arrays.asList("d", "BaBb", "cab", "aB"); // La lista así creada es inmutable
-    	String result = list
-    						.stream() // Las operaciones agregadas en Java 8 se hacen sobre streams, no sobre colecciones
-    						.filter(s -> s.length() % 2 == 0) // Quedarnos con las de longitud par						
-    						.map(String::toUpperCase) // Pasar a mayúsculas
-    						.sorted() // Ordenar después de pasar a mayúsculas, para que sea orden alfabético
-    						.collect(Collectors.joining()); // Convertir en String concatenando las que quedan    	
-	    System.out.println(result);
-  }
+public class Pipeline {
+	
+	public static void main(String[] argv) throws Exception {
+		List<String> list = Arrays.asList("d", "BaBb", "cab", "aB"); // La lista así creada es inmutable
+		String result = list
+				.stream() // Las operaciones agregadas en Java 8 se hacen sobre streams, no sobre colecciones
+				.filter(s -> s.length() % 2 == 0) // Quedarnos con las de longitud par						
+				.map(String::toUpperCase) // Pasar a mayúsculas
+    			.sorted() // Ordenar después de pasar a mayúsculas, para que sea orden alfabético
+    			.collect(Collectors.joining()); // Convertir en String concatenando las que quedan    	
+		System.out.println(result);
+	}
 }
